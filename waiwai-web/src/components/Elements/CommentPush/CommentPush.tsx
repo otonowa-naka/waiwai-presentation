2800ad91544eaa5694b8286a86c8a1782ea6424b
誠 中野 <otonowa.naka@gmail.com>
2022-11-06 13:15:54 +0900

    ESLintの指摘修正

--- waiwai-web/src/components/Elements/CommentPush/CommentPush.tsx
+++ waiwai-web/src/components/Elements/CommentPush/CommentPush.tsx
@@ -1,33 +1,32 @@
-import {FC, useState, ChangeEvent} from 'react'
+import { FC, useState, ChangeEvent } from 'react'
 import { SButton } from '../SButton'
 
-//　コメント送信コンポート
+// コメント送信コンポート
 
-type Porps = 
-{
-    PushComment:(comment:string)=>void  //コメント送信関数
-}
+type Porps =
+    {
+        PushComment: (comment: string) => void  //コメント送信関数
+    }
 
 
-export const CommentPush:FC<Porps> = (props) =>
-{
-    const {PushComment} = props
+export const CommentPush: FC<Porps> = (props) => {
+    const { PushComment } = props
 
-    // 送信コメント　State
+    // 送信コメント State
     const [text, setText] = useState<string>("")
- 
-      // テキストボックス入力時に入力内容をStateに保存
+
+    // テキストボックス入力時に入力内容をStateに保存
     const OnChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)
 
-    //　送信ボタンクリック
-    const　OnClickAdd = ()=>{
+    // 送信ボタンクリック
+    const OnClickAdd = () => {
         PushComment(text)
         setText("")
     }
 
-    return( 
-     <div>
-        <input type="text" value={text} onChange={OnChangeText}></input>
-        <SButton onClick={OnClickAdd}>送信</SButton>
-    </div>)
+    return (
+        <div>
+            <input type="text" value={text} onChange={OnChangeText}></input>
+            <SButton onClick={OnClickAdd}>送信</SButton>
+        </div>)
 }
