ee882ddedc1e5b7e910f8df2083fa5b8abf62137
誠 中野 <otonowa.naka@gmail.com>
2022-11-07 13:25:10 +0900

    テストの追加

--- waiwai-web/src/index.tsx
+++ waiwai-web/src/index.tsx
@@ -1,27 +1,30 @@
 import ReactDOM from 'react-dom/client';
-import {App} from './components/App';
+import { App } from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
-import { store} from './store'
+import { store } from './store'
 
 //FireBase関連のImport
 import { firebaseConfig } from './firebaseConfig'
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 
 // fireBaseの初期化処理
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseDb = getDatabase();
 
+// Point to the RTDB emulator running on localhost.
+connectDatabaseEmulator(firebaseDb, "localhost", 9000);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    // Reduxを利用する為にProviderで囲む
-    <Provider store={store}>
-      <App />
-    </Provider>
+  // Reduxを利用する為にProviderで囲む
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
